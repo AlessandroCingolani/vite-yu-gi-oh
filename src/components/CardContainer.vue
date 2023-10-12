edbda4a1c3f1824ef9491d55afe999e9173caa94
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-10-12 15:07:04 +0200

    [ add ] component cards result main

---
+++ src/components/CardContainer.vue
@@ -0,0 +1,24 @@
+<script>
+import Cards from './partials/Cards.vue';
+export default {
+  name:'CardContainer',
+  components:{
+    Cards
+  }
+}
+</script>
+
+
+<template>
+  <div class="container">
+    <div class="row">
+      <Cards/>
+    </div>
+  </div>
+</template>
+
+
+
+<style>
+
+</style>
---
+++ src/components/Result.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  name:'Result'
+}
+</script>
+
+
+<template>
+  <div>Result</div>
+</template>
+
+
+
+<style>
+
+</style>
