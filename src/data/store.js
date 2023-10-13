f32a7a368b9952be6222746a409483b5180b23ea
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-10-13 17:12:37 +0200

    [ add ] Api archtype

--- src/data/store.js
+++ src/data/store.js
@@ -2,6 +2,7 @@ import { reactive } from 'vue';
 
 export const store = reactive({
   apiUrl:'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=100&offset=0',
+  typeUrl:'https://db.ygoprodeck.com/api/v7/archetypes.php',
   cardList: [],
   research:'',
   isLoading: false,
