252bc24f77f334bab6573d898f29ad173443e3ee
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-10-13 22:04:22 +0200

    [ change ] API num display cards

--- src/data/store.js
+++ src/data/store.js
@@ -1,7 +1,7 @@
 import { reactive } from 'vue';
 
 export const store = reactive({
-  apiUrl:'https://db.ygoprodeck.com/api/v7/cardinfo.php',
+  apiUrl:'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=150&offset=0',
   typeUrl:'https://db.ygoprodeck.com/api/v7/archetypes.php',
   cardList: [],
   research:null,
